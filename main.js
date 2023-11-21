302cede38250a812ee3b5d7b241c363f11e9a554
Zojo24 <hchoi2894@gmail.com>
2023-11-21 18:49:26 +0900

    section 추가 및 레이아웃 정리

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //swiper//
-new Swiper('.main__content .swiper-container', {
+new Swiper('.main__visual .swiper-container', {
   slidesPreVeiw: 1,
   centerdSlides: true,
   loop: true,
@@ -7,29 +7,29 @@ new Swiper('.main__content .swiper-container', {
     delay: 4000 
   },
   pagination: {
-    el: '.main__content .swiper-pagination',
+    el: '.main__visual .swiper-pagination',
     clickable: true
   },
   navigation: {
-    prevEl: 'main__content .swiper-prev',
-    nextEl: 'main__content .swiper-next'
+    prevEl: 'main__visual .swiper-prev',
+    nextEl: 'main__visual .swiper-next'
   }
 });
 
-// new Swiper('.content__new .swiper-container', {
-//   slidesPreVeiw: 4,
-//   spaceBetween: 10,
-//   centerdSlides: true,
-//   loop: true,
-//   autoplay: {
-//     delay: 5000 
-//   },
-//   pagination: {
-//     el: '.content__new .swiper-pagination',
-//     clickable: true
-//   },
-//   navigation: {
-//     prevEl: '.content__new .swiper-prev',
-//     nextEl: '.content__new .swiper-next'
-//   }
-// });
+new Swiper('.main__new .swiper-container', {
+  slidesPreVeiw: 4,
+  spaceBetween: 10,
+  centerdSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000 
+  },
+  pagination: {
+    el: '.main__new .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.main__new .swiper-prev',
+    nextEl: '.main__new .swiper-next'
+  }
+});
